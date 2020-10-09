5e7e2783740c2e086b7c0e202ec0420baf1b9425
Tyler Elston <tyler.elston7@gmail.com>
2020-10-09 17:02:12 +0000

    piece placement and board row state evaluation

--- main.py
+++ main.py
@@ -13,11 +13,10 @@ white = 255, 255, 255
 
 board = [[0 for num in range(7)] for row in range(6)]
 
-
 screen = pygame.display.set_mode(size)
 
 # display game board
-def display_board(screen, board):
+def displayBoard(screen, board):
   pygame.draw.rect(screen, grey, (10, 10, 560, 560))
   colour = white
   x = 15
@@ -34,10 +33,50 @@ def display_board(screen, board):
     x = 15
     y += 40
 
-display_board(screen, board)
+# place piece
+def placePiece(board, col, colour):
+  print('PLACING')
+  row = 0
+  while row < 5 and board[row][col] == 0:
+    if board[row][col] == 0 and board[row+1][col] == 0:
+      row += 1
+    else:
+      break
+  board[row][col] = colour
+  evaluateState(board)
+
+# evaluates board for a winning state
+def evaluateState(board):
+  consecutive = 1
+  # check for row win
+  for col in range(len(board[0])):
+      prevColour = 0
+      for row in range(len(board)):
+        colour = board[row][col]
+        if colour != 0 and prevColour == colour:
+          consecutive += 1
+        else:
+          consecutive = 1
+        prevColour = colour
+
+        if consecutive == 4:
+          print("Win:", colour)
+          return True
+        
+
+  # check for column win
+
+  # check for diagonal win
+
 pygame.display.update()
 
+
+
+
+
+displayBoard(screen, board)
 while 1:
+  
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
